a97834831bf7c1a09d9e326407cc3137702d986d
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-03-05 15:06:30 +0530

    Added:inputs and constructors to create contact

--- Address_Book.js
+++ Address_Book.js
@@ -1,1 +1,31 @@
 console.log("Welcome to Address Book System");
+class Contact {
+  firstName;
+  lastName;
+  address;
+  city;
+  state;
+  zip;
+  phoneNumber;
+  email;
+  
+  constructor(firstName, lastName, address, city, state, zip, phoneNumber, email){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.city = city;
+    this.state = state;
+    this.zip = zip;
+    this.phoneNumber = phoneNumber;
+    this.email = email;
+  }
+  toString() {
+    return "First name: " + this.firstName + "\nLast name: " + this.lastName + 
+           "\nAddress: " + this.address +  "\nCity: " + this.city + 
+           "\nState: " + this.state + "\nZip: " + this.zip + 
+          "\nPhone number: " + this.phoneNumber + "\nEmail: " + this.email;
+  }
+}
+let contact = new Contact("Dnyaneshwari","Gaikwad","vidyaNagar","Solapur","Maharashtra",12345,8767654567,"gaikwad5@gmail.com");
+console.log(contact.toString());
+
